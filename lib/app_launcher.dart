76335ddf7ee39e694581916fbd8c35e851e6c436
Akora-IngDKB <debrahkwesibuabeng2@gmail.com>
2020-09-13 22:00:50 +0000

    Declare top level functions

--- lib/app_launcher.dart
+++ lib/app_launcher.dart
@@ -5,16 +5,16 @@ import 'package:flutter/services.dart';
 class AppLauncher {
   static const MethodChannel _channel = const MethodChannel('app_launcher');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
+  static Future<void> openApp({String androidApplicationId}) async {
+    assert(
+      androidApplicationId != null && androidApplicationId.isNotEmpty,
+      "[androidApplicationId] cannot be null or empty",
+    );
 
-  static Future<void> showToast(String message) async {
     await _channel.invokeMethod(
-      'showToast',
+      'openApp',
       <String, Object>{
-        'message': message,
+        'applicationId': androidApplicationId,
       },
     );
   }
