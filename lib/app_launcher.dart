8e195e9b91d42bae3e6d90b032577ad96afeb99f
Akora-IngDKB <debrahkwesibuabeng2@gmail.com>
2020-09-13 22:52:40 +0000

    Added documentation to function

--- lib/app_launcher.dart
+++ lib/app_launcher.dart
@@ -5,6 +5,10 @@ import 'package:flutter/services.dart';
 class AppLauncher {
   static const MethodChannel _channel = const MethodChannel('app_launcher');
 
+  /// Attempt to open the app on the device with the [`androidApplicationId`].
+  /// [`androidApplicationId`] should not be empty or null.
+  ///
+  /// Will display a toast if no app is found.
   static Future<void> openApp({String androidApplicationId}) async {
     assert(
       androidApplicationId != null && androidApplicationId.isNotEmpty,
