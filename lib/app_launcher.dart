fc413b2d9b6158082d9c78e2cf115d618e42ad2a
Akora-IngDKB <debrahkwesibuabeng2@gmail.com>
2020-09-13 23:38:37 +0000

    Top level code for hasApp

--- lib/app_launcher.dart
+++ lib/app_launcher.dart
@@ -5,10 +5,32 @@ import 'package:flutter/services.dart';
 class AppLauncher {
   static const MethodChannel _channel = const MethodChannel('app_launcher');
 
-  /// Attempt to open the app on the device with the [`androidApplicationId`].
+  /// Check if the app with [`androidApplicationId`] is installed on the device.
+  ///
+  /// Returns `true` if found or `false` otherwise.
+  ///
   /// [`androidApplicationId`] should not be empty or null.
+  static Future<bool> hasApp({String androidApplicationId}) async {
+    assert(
+      androidApplicationId != null && androidApplicationId.isNotEmpty,
+      "[androidApplicationId] cannot be null or empty",
+    );
+
+    final result = await _channel.invokeMethod<bool>(
+      'hasApp',
+      <String, Object>{
+        'applicationId': androidApplicationId,
+      },
+    );
+
+    return result;
+  }
+
+  /// Attempt to open the app on the device with the [`androidApplicationId`].
   ///
   /// Will display a toast if no app is found.
+  ///
+  /// [`androidApplicationId`] should not be empty or null.
   static Future<void> openApp({String androidApplicationId}) async {
     assert(
       androidApplicationId != null && androidApplicationId.isNotEmpty,
