6079747168b1f58b76bb12ca7689e213e90fe5c7
Akora-IngDKB <debrahkwesibuabeng2@gmail.com>
2020-09-13 22:02:32 +0000

    Implement example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,47 +1,12 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:app_launcher/app_launcher.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await AppLauncher.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -51,13 +16,24 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Center(
           child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              Text('Running on: $_platformVersion\n'),
               RaisedButton(
                 onPressed: () async {
-                  await AppLauncher.showToast('Hello There Platform Channel');
+                  await AppLauncher.openApp(
+                    androidApplicationId: "com.whatsapp",
+                  );
                 },
-                child: Text('Show Toast'),
+                child: Text('Open WhatsApp'),
+              ),
+              SizedBox(height: 16),
+              RaisedButton(
+                onPressed: () async {
+                  await AppLauncher.openApp(
+                    androidApplicationId: "sth.apply.wrongapp",
+                  );
+                },
+                child: Text('Open Something Else'),
               ),
             ],
           ),
