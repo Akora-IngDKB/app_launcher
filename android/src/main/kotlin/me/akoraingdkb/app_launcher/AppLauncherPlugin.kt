fe0b12f43a89bb6ae41167d09b8fa0bf0b6410c9
Akora-IngDKB <debrahkwesibuabeng2@gmail.com>
2020-09-14 11:32:02 +0000

    Open Google Play if app is installed

--- android/src/main/kotlin/me/akoraingdkb/app_launcher/AppLauncherPlugin.kt
+++ android/src/main/kotlin/me/akoraingdkb/app_launcher/AppLauncherPlugin.kt
@@ -3,6 +3,7 @@ package me.akoraingdkb.app_launcher
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.widget.Toast
 import androidx.annotation.NonNull
 
@@ -37,11 +38,19 @@ class AppLauncherPlugin : FlutterPlugin, MethodCallHandler {
                     if (intent != null) {
                         context.startActivity(intent)
                     } else {
-                        Toast.makeText(context, "No app was found with the application ID ${call.argument<String>("applicationId")}", Toast.LENGTH_LONG).show()
+                        Toast.makeText(context, "No app was found with the application ID ${call.argument<String>("applicationId")}\nOpening Google Play...", Toast.LENGTH_LONG).show()
+
+                        val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=${call.argument<String>("applicationId")}"))
+                        i.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                        context.startActivity(i)
                     }
                 } catch (e: PackageManager.NameNotFoundException) {
-                    Toast.makeText(context, "No app was found with the application ID ${call.argument<String>("applicationId")}", Toast.LENGTH_LONG).show()
+                    Toast.makeText(context, "No app was found with the application ID ${call.argument<String>("applicationId")}\nOpening Google Play...", Toast.LENGTH_LONG).show()
                     result.error("APP_NOT_FOUND", "No app was found with the specified application ID", "Please specify a correct application ID");
+
+                    val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=${call.argument<String>("applicationId")}"))
+                    i.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    context.startActivity(i)
                 }
             }
             "hasApp" -> {
