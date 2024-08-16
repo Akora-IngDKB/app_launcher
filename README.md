45959a426c14594b44fcfb512c422a235786e0f3
Gopal Chitaure <gochit01@wsc.edu>
2024-08-16 09:54:16 +0545

    Update better example UI, updated versions

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 # app_launcher
+
 [![Pub](https://img.shields.io/pub/v/app_launcher.svg?style=flat-square&logo=dart&logoColor=white&color=blue)](https://pub.dev/packages/app_launcher)
 [![License](https://img.shields.io/badge/license-MIT-purple.svg?style=flat-square)](LICENSE)
 
-A flutter plugin that launches an Android or iOS installed on the device using the application ID.  
+A flutter plugin that launches an Android or iOS installed on the device using the application ID.
+
+[![Lice![app_launcher_example](https://github.com/user-attachments/assets/6ef667f8-fcd3-431a-a173-f8c0e8d08784)
 
 #### NOTICE
+
 > Supports only Android as of now. PRs for iOS support will be very appreciated.
 
 ## Usage
+
 Simply call the `openApp` static method and pass in your application ID as a String:
+
 ```dart
 RaisedButton(
   onPressed: () async {
@@ -19,21 +25,23 @@ RaisedButton(
   child: Text('Open WhatsApp'),
 ),
 ```
-If app is not installed, it'll attempt to open the app details on Google Play.  
 
+If app is not installed, it'll attempt to open the app details on Google Play.
 
 You can also check if the app is installed on the device.
+
 ```dart
 final bool isInstalled = await AppLauncher.openApp(
   androidApplicationId: "com.whatsapp",
 );
 ```
-`isInstalled` will be `true` if found and `false` otherwise.  
 
+`isInstalled` will be `true` if found and `false` otherwise.
 
-**Note: the `androidApplicationId` must neither be empty nor `null`**.  
+**Note: the `androidApplicationId` must neither be empty nor `null`**.
 
 ## TODO
+
 - [x] Android Support.
 - [x] Open Google Play when app not found.
 - [ ] iOS Support.
@@ -41,10 +49,13 @@ final bool isInstalled = await AppLauncher.openApp(
 - [ ] Support passing arguments.
 
 ## Example
+
 Check the examples [tab](https://pub.dev/packages/app_launcher/example) or take a look at the example [file](https://github.com/Akora-IngDKB/app_launcher/blob/master/example/lib/main.dart).
 
 ## Contribution
-This project is fully open-source. Feel free to open issues when you realize one and PRs are welcome as well.  
+
+This project is fully open-source. Feel free to open issues when you realize one and PRs are welcome as well.
 
 ## License
+
 This project has been licensed under the MIT License. Check the [LICENSE](LICENSE) file for the details.
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:app_launcher/app_launcher.dart';
 
 void main() {
@@ -10,45 +9,131 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('App Launcher Example'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton(
-                onPressed: () async {
-                  await AppLauncher.openApp(
-                    androidApplicationId: "com.whatsapp",
-                  );
-                },
-                child: Text('Open WhatsApp'),
-              ),
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () async {
-                  final value = await AppLauncher.hasApp(
-                    androidApplicationId: "org.videolan.vlc",
-                  );
+      debugShowCheckedModeBanner: false,
+      home: LauncherScreen(),
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        fontFamily: 'Arial',
+      ),
+    );
+  }
+}
 
-                  print(value);
-                },
-                child: Text('Check for VLC'),
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  await AppLauncher.openApp(
-                    androidApplicationId: "org.videolan.vlc",
-                  );
-                },
-                child: Text('Open for VLC'),
-              ),
-            ],
+class LauncherScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'App Launcher Example',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.deepOrangeAccent,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.orangeAccent, Colors.deepOrange],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                _buildCustomButton(
+                  context,
+                  title: 'Open Ludo Game',
+                  onPressed: () async {
+                    await AppLauncher.openApp(
+                      androidApplicationId: "com.karuwaapps.ludo",
+                    );
+                  },
+                ),
+                SizedBox(height: 16),
+                _buildCustomButton(
+                  context,
+                  title: 'Check for Ludo Game',
+                  onPressed: () async {
+                    final isInstalled = await AppLauncher.hasApp(
+                      androidApplicationId: "com.karuwaapps.ludo",
+                    );
+
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          isInstalled
+                              ? 'Ludo Game is installed'
+                              : 'Ludo Game is not installed',
+                        ),
+                        backgroundColor: Colors.black87,
+                      ),
+                    );
+                  },
+                ),
+                SizedBox(height: 16),
+                _buildCustomButton(
+                  context,
+                  title: 'Check for VLC',
+                  onPressed: () async {
+                    final isInstalled = await AppLauncher.hasApp(
+                      androidApplicationId: "org.videolan.vlc",
+                    );
+
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          isInstalled
+                              ? 'VLC is installed'
+                              : 'VLC is not installed',
+                        ),
+                        backgroundColor: Colors.black87,
+                      ),
+                    );
+                  },
+                ),
+                SizedBox(height: 16),
+                _buildCustomButton(
+                  context,
+                  title: 'Open VLC',
+                  onPressed: () async {
+                    await AppLauncher.openApp(
+                      androidApplicationId: "org.videolan.vlc",
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildCustomButton(BuildContext context,
+      {required String title, required VoidCallback onPressed}) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(vertical: 16),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.deepOrangeAccent,
+        shadowColor: Colors.deepOrange,
+        elevation: 10,
+        textStyle: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ),
+      child: Text(title),
     );
   }
 }
